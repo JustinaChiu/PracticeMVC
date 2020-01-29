6dd107ba93c5754d7be690ae02a3ef61c56c21a3
JustinaChiu <jjj864699@gmail.com>
2020-01-30 01:29:22 +0800

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using PracticeMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PracticeMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var rand = new Random();
+            DateTime startDate = new DateTime(2018, 1, 1);
+            List<Records> recordList = new List<Records>();
+            for (int i = 0; i < 100; i++)
+            {
+                Records item = new Records();
+                item.No = i + 1;
+                item.Type = rand.Next(2);
+                item.Date = startDate;
+                item.Amount = rand.Next(10001);
+                startDate = startDate.AddDays(rand.Next(3));
+                recordList.Add(item);
+            }
+            ViewBag.RecordList = recordList;
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Models/RecordViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PracticeMVC.Models
+{
+    public class Records
+    {
+        public int No { get; set; }
+        public int Type { get; set; }
+        public DateTime Date { get; set; }
+        public int Amount { get; set; }
+    }
+
+    public class RecordViewModel
+    {
+        public List<Records> RecordList { get; set; }
+    }
+}
